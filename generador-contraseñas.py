af176f9013e9d24069c914586ac2d12749c7c461
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 11:40:09 +0100

    probamos el primer comando

---
+++ generador-contraseñas.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+longitud = int(input("Ingrese la longitud de la contraseña: "))
+caracteres = string.ascii_letters + string.digits + string.punctuation  
+
+print(caracteres)
