bd5043766b36ec04b4ddfbe0dbf1a41cf7c2fad8
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 11:46:05 +0100

    probamos si el generador funciona correctamente

--- generador-contraseñas.py
+++ generador-contraseñas.py
@@ -1,7 +1,14 @@
 import string
 import random
 
+#establecemos longitud de la contraseña
 longitud = int(input("Ingrese la longitud de la contraseña: "))
+
+#genera los caracteres para usar en la contraseña
 caracteres = string.ascii_letters + string.digits + string.punctuation  
 
-print(caracteres)
+#genera aleatoriamente la contraseña con la longitud determinada
+contraseña = "".join(random.choice(caracteres) for i in range(longitud))
+
+#imprime la contraseña generada
+print("La contraseña generada es: " + contraseña)
